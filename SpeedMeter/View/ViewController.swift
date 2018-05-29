2504c62e1f0ab2afb841735da3981413ce988b53
Mustafa GUNES <gunes149@gmail.com>
2018-05-29 23:28:25 +0300

    imported RevealingSplashView

--- SpeedMeter/View/ViewController.swift
+++ SpeedMeter/View/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import RevealingSplashView
 
 class ViewController: UIViewController {
+    
+    let splashView = RevealingSplashView(iconImage: UIImage (named: "Icon-3x")!, iconInitialSize: CGSize (width: 70, height: 70), backgroundColor: UIColor.white)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(splashView)
+        splashView.animationType = .heartBeat
+        splashView.startAnimation()
+        
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { (timer) in
+            self.splashView.finishHeartBeatAnimation()
+        }
     }
 }
 
